650653f0cbbf6b30e8259e4bd900499e65c4a73d
Helio S. Junior <hj_junior@hotmail.com>
2018-12-07 11:21:19 -0200

    Refactored with items card

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -4,6 +4,7 @@ import 'package:flutter_furniture_app/src/widgets/tab_bar/tab_bar.dart';
 import 'package:flutter_furniture_app/src/widgets/list_items/item_card.dart';
 
 class Home extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) => Scaffold(
     body: ListView(
@@ -11,37 +12,26 @@ class Home extends StatelessWidget {
         Column(
           children: <Widget>[
             Header(),
-            Stack(
-              children: <Widget>[
-                SizedBox(height: 10.0,),
-                Material(
-                  elevation: 1.0,
-                  child: Container(
-                    height: 75.0,
-                    color: Colors.white,
-                  ),
-                ),
-                TabBarMenu(),
-              ],
+            TabBarMenu(),
+            ItemCard(
+              title: 'FinnNavian',
+              imgPath: 'assets/images/ottoman.jpg',
+              isFavorite: false
             ),
             ItemCard(
-                title: 'FinnNavian',
-                imgPath: 'assets/images/ottoman.jpg',
-                isFavorite: false
+              title: 'FinnNavian',
+              imgPath: 'assets/images/anotherchair.jpg',
+              isFavorite: true
             ),
             ItemCard(
-                title: 'FinnNavian',
-                imgPath: 'assets/images/anotherchair.jpg',
-                isFavorite: true
-            ),
-            ItemCard(
-                title: 'FinnNavian',
-                imgPath: 'assets/images/chair.jpg',
-                isFavorite: true
+              title: 'FinnNavian',
+              imgPath: 'assets/images/chair.jpg',
+              isFavorite: true
             )
           ],
         )
       ],
     ),
   );
+
 }
--- lib/src/widgets/tab_bar/tab_bar.dart
+++ lib/src/widgets/tab_bar/tab_bar.dart
@@ -2,25 +2,38 @@ import 'package:flutter/material.dart';
 import 'item_tab_bar.dart';
 
 class TabBarMenu extends StatelessWidget {
+
   @override
-  Widget build(BuildContext context) => Row(
-    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+  Widget build(BuildContext context) => Stack(
     children: <Widget>[
-      TabBarMenuItem(
-        assetName: 'sofas.png',
-        text: 'Sofas'
+      SizedBox(height: 10.0,),
+      Material(
+        elevation: 1.0,
+        child: Container(
+          height: 75.0,
+          color: Colors.white,
+        ),
       ),
-      TabBarMenuItem(
-        assetName: 'wardrobe.png',
-        text: 'Wardrobe'
-      ),
-      TabBarMenuItem(
-        assetName: 'desks.png',
-        text: 'Desk'
-      ),
-      TabBarMenuItem(
-        assetName: 'dressers.png',
-        text: 'Dresser'
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          TabBarMenuItem(
+              assetName: 'sofas.png',
+              text: 'Sofas'
+          ),
+          TabBarMenuItem(
+              assetName: 'wardrobe.png',
+              text: 'Wardrobe'
+          ),
+          TabBarMenuItem(
+              assetName: 'desks.png',
+              text: 'Desk'
+          ),
+          TabBarMenuItem(
+              assetName: 'dressers.png',
+              text: 'Dresser'
+          ),
+        ],
       ),
     ],
   );
